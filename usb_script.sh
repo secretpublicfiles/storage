2ceb739551c241dc1acee4c804be1ec17c035b45
secretpublicfiles <88037973+secretpublicfiles@users.noreply.github.com>
2021-07-27 15:36:10 +0300

    rc.local url
    rc.local için url verildi

--- usb_script.sh
+++ usb_script.sh
@@ -5,7 +5,7 @@ echo "Be careful: It applies changes to your system files!"
 
 sudo echo "dtoverlay=dwc2" >> /boot/config.txt
 wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/cmdline.txt' -O cmdline.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=19WCRsswr3Lr4PBNrNz2yoy7PZ5HfRu-y' -O rc.local
+wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/rc.local' -O rc.local
 sudo cp cmdline.txt /boot
 sudo rm cmdline.txt
 sudo dd bs=1M if=/dev/zero of=/piusb.bin count=16384
