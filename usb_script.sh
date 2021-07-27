299970a7b792695562bf12566f954511e3d061c5
secretpublicfiles <88037973+secretpublicfiles@users.noreply.github.com>
2021-07-27 15:35:07 +0300

    cmdline url
    cmdline için github linki verildi

--- usb_script.sh
+++ usb_script.sh
@@ -4,7 +4,7 @@ echo "Be careful: It applies changes to your system files!"
 
 
 sudo echo "dtoverlay=dwc2" >> /boot/config.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=10AZEtoUlm_PrZOIUxxUMrnz0LBNDhkbr' -O cmdline.txt
+wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/cmdline.txt' -O cmdline.txt
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=19WCRsswr3Lr4PBNrNz2yoy7PZ5HfRu-y' -O rc.local
 sudo cp cmdline.txt /boot
 sudo rm cmdline.txt
