907aae3050e7a9b42a348276864e705e7ef32c78
secretpublicfiles <88037973+secretpublicfiles@users.noreply.github.com>
2021-07-28 19:26:07 +0300

    Update usb_script.sh

--- usb_script.sh
+++ usb_script.sh
@@ -4,14 +4,14 @@ echo "Be careful: It applies changes to your system files!"
 
 
 sudo echo "dtoverlay=dwc2" >> /boot/config.txt
-wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/cmdline.txt' -O cmdline.txt
+#wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/cmdline.txt' -O cmdline.txt
 wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/rc.local' -O rc.local
 sudo sed -i 's/rootwait/rootwait modules-load=dwc2/' /boot/cmdline.txt
-# sudo dd bs=1M if=/dev/zero of=/piusb.bin count=10240
-# sudo mkdosfs "/piusb.bin"
-# sudo mkdir /mnt/usb_share
-# sudo echo "/piusb.bin /mnt/usb_share vfat users,umask=000 0 2" >> /etc/fstab
-# sudo mount -a
-# sudo cp rc.local /etc
-# sudo rm rc.local
+sudo dd bs=1M if=/dev/zero of=/piusb.bin count=16384
+sudo mkdosfs "/piusb.bin"
+sudo mkdir /mnt/usb_share
+sudo echo "/piusb.bin /mnt/usb_share vfat users,umask=000 0 2" >> /etc/fstab
+sudo mount -a
+sudo cp rc.local /etc
+sudo rm rc.local
 echo "Please reboot the system!"
