694d2aa7dc1fa59ffa66e7e801bfeeeb1236463f
secretpublicfiles <88037973+secretpublicfiles@users.noreply.github.com>
2021-07-28 19:16:58 +0300

    Update usb_script.sh

--- usb_script.sh
+++ usb_script.sh
@@ -6,8 +6,7 @@ echo "Be careful: It applies changes to your system files!"
 sudo echo "dtoverlay=dwc2" >> /boot/config.txt
 wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/cmdline.txt' -O cmdline.txt
 wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/rc.local' -O rc.local
-sudo cp cmdline.txt /boot
-sudo rm cmdline.txt
+sudo sed -i 's/rootwait/rootwait modules-load=dwc2/' /boot/cmdline.txt
 # sudo dd bs=1M if=/dev/zero of=/piusb.bin count=10240
 # sudo mkdosfs "/piusb.bin"
 # sudo mkdir /mnt/usb_share
