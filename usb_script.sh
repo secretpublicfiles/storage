1a144c5d53a7a154c835137f3e11a3531cf852a8
secretpublicfiles <88037973+secretpublicfiles@users.noreply.github.com>
2022-03-25 15:55:55 +0300

    8192

--- usb_script.sh
+++ usb_script.sh
@@ -7,7 +7,7 @@ sudo echo "dtoverlay=dwc2" >> /boot/config.txt
 #wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/cmdline.txt' -O cmdline.txt
 wget 'https://raw.githubusercontent.com/secretpublicfiles/storage/main/rc.local' -O rc.local
 sudo sed -i 's/rootwait/rootwait modules-load=dwc2/' /boot/cmdline.txt
-sudo dd bs=1M if=/dev/zero of=/piusb.bin count=8000
+sudo dd bs=1M if=/dev/zero of=/piusb.bin count=8192
 sudo mkdosfs "/piusb.bin"
 sudo mkdir /mnt/usb_share
 sudo echo "/piusb.bin /mnt/usb_share vfat users,umask=000 0 2" >> /etc/fstab
